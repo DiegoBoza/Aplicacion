50a3f0d19af12140b27baf16e337369548fb9c7f
DiegoBoza <120494987+DiegoBoza@users.noreply.github.com>
2022-12-16 01:24:25 -0500

    Add files via upload

---
+++ Principal.py
@@ -0,0 +1,40 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title ="Multipage App",
+)
+st.write("""
+# Universidad Nacional de San Agustín de Arequipa
+# Escuela Profesional de Ingeniería de Telecomunicaciones
+""")
+
+st.write("""![varianz](https://empleoz.com/media/logo/592546c81ca7507c29e72496.png)""")
+st.sidebar.success("Select a page above.")
+
+st.write("""
+### Ingeniero Renzo Bolivar - Docente DAIE
+### Curso : Computación 1
+### GRUPO C - Nº3
+### Alumnos:
+    Boza Anampa Diego Alexander
+    Huamani Quispe Elisban
+    Chambi Quispe Josue Ronaldo
+    Ccoa Arque Ronaldo
+    Castellanos Morales Luis Felipe Lucciani
+""")
+
+st.write("""
+## INVESTIGACIÓN FORMATIVA
+## PROYECTO FINAL
+## PYTHON - Inteligencia Artificial
+""")
+
+st.write("""
+## OBJETIVOS
+Los Objetivos de la investigación formativa son:
+
+- **Competencia Comunicativa** Presentación de sus resultados con lenguaje de programación Python utilizando los archivos Jupyter Notebook.
+- **Competencia Aprendizaje**: con las aptitudes en **Descomposición** (desarticular el problema en pequeñas series de soluciones), **Reconocimiento de Patrones** (encontrar simulitud al momento de resolver problemas), **Abstracción** (omitir información relevante), **Algoritmos** (pasos para resolución de un problema).
+- **Competencia de Trabajo en Equipo**: exige habilidades individuales y grupales orientadas a la cooperación, planificación, coordinación, asignación de tareas, cumplimiento de tareas y solución de conflictos en pro de un trabajo colectivo, utilizando los archivos Jupyter Notebook los cuales se sincronizan en el servidor Gitlab con comandos Git.
+
+""")
